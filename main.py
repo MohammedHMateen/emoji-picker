2009bfd6e3adf976c18161b820d54b7331fc3f9a
Jordane Lew <jordane.lew@gmail.com>
2019-12-25 09:55:10 +1300

    Visual tweaks and enter selects hovered emoji
    Hovered emoji can not yet be selected differently (tab through coming?)

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from kivy.uix.scatter import Scatter
 from kivy.uix.image import AsyncImage
 
 MAX_FILTERED_RESULTS = 28
-WINDOW_WIDTH = 410
+WINDOW_WIDTH = 420
 WINDOW_HEIGH = 280
 
 # TODO: Convert into preferences
@@ -23,7 +23,8 @@ CLOSE_ON_SELECTION = True
 class EmojiKeyboard(BoxLayout):
 
     orientation = 'vertical'
-    spacing = 10
+    spacing = 5
+    padding = 5, 5, 5, 5
 
     def __init__(self, **kwargs):
         super(EmojiKeyboard, self).__init__(**kwargs)
@@ -31,16 +32,21 @@ class EmojiKeyboard(BoxLayout):
         self.emoji_dictionary = EmojiDictionary()
 
         self.emoji_grid = EmojiGrid()
-        self.filterInput = TextInput(multiline=False, size_hint_y=None, height=40)
+        self.filterInput = TextInput(multiline=False, size_hint_y=None, height=35)
         self.filterInput.focus = True
-        self.filterInput.bind(text=self.on_filter_text)
+        self.filterInput.font_size = 18
+        self.filterInput.bind(text=self.on_filter_text, on_text_validate=self.on_enter)
 
         self.add_widget(self.filterInput)
         self.add_widget(self.emoji_grid)
 
     def on_filter_text(self, instance, value):
+        print(value)
         self.emoji_grid.items = self.emoji_dictionary.search(
             value, max_items=MAX_FILTERED_RESULTS)
+
+    def on_enter(self, instance):
+        self.emoji_grid.select_hovered_emoji()
 
 
 class EmojiGrid(StackLayout):
@@ -54,28 +60,43 @@ class EmojiGrid(StackLayout):
     def on_items(self, *args):
         self.clear_widgets()
 
-        for item in self.items:
-            self.add_widget(EmojiButton(item))
+        def item_to_widget(item):
+            return EmojiButton(item)
+
+        widgets = list(map(item_to_widget, self.items))
+        for widget in widgets:
+            self.add_widget(widget)
+        if len(widgets) > 0:
+            widgets[0].hover()
+            self.selected_widget = widgets[0]
+
+    def select_hovered_emoji(self):
+        self.selected_widget.on_release()
 
 
 class EmojiButton(Button):
 
-    font_size = 20
     size_hint = (None, None)
 
-    def __init__(self, emoji, **kwargs):
+    def __init__(self, emoji, selected=False, **kwargs):
         super(EmojiButton, self).__init__(**kwargs)
+
         self.emoji = emoji
+        self.selected = selected
 
         self.image = AsyncImage(source=emoji['path'])
         self.add_widget(self.image)
 
         self.bind(size=self._update_image, pos=self._update_image)
         self.size = 50, 50
+        self.background_color = 0, 0, 0, 0
 
     def _update_image(self, instance, value):
         self.image.center = instance.center
-        self.image.size = 40, 40
+        self.image.size = 35, 35
+
+    def hover(self):
+        self.background_color = 1, 1, 1, 0.5
 
     def on_release(self):
         emoji_code = self.emoji['emoji']
@@ -100,4 +121,5 @@ class EmojiKeyboardApp(App):
 
 if __name__ == '__main__':
     Window.size = (WINDOW_WIDTH, WINDOW_HEIGH)
+    Window.clearcolor = 0.15, 0.15, 0.15, 1
     EmojiKeyboardApp().run()
